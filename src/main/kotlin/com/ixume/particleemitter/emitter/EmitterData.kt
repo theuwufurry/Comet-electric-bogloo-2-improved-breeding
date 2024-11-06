e22e31e88c59a355235e82cd9638a6b350a336c7
russian <dominusfinem@gmail.com>
2024-11-06 07:46:12 -0800

    emitter data random

--- src/main/kotlin/com/ixume/particleemitter/emitter/EmitterData.kt
+++ src/main/kotlin/com/ixume/particleemitter/emitter/EmitterData.kt
@@ -3,10 +3,11 @@ package com.ixume.particleemitter.emitter
 import org.bukkit.World
 import org.joml.Quaterniond
 
-data class EmitterData(var age: Double = 0.0, var world: World? = null, var rotation: Quaterniond? = Quaterniond()) {
+data class EmitterData(var age: Double = 0.0, var world: World? = null, var rotation: Quaterniond? = Quaterniond(), var random: Double = Math.random()) {
     fun copyFrom(other: EmitterData) {
         age = other.age
         world = other.world
         rotation = other.rotation
+        random = other.random
     }
 }
