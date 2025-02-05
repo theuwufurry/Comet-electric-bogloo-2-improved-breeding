67cb266f538386bb664c7f7a7dfb2a78c44b9991
MrLarkyy_ <studiotadikl@gmail.com>
2025-02-05 21:51:46 +0100

    Refactor MEGParent into an interface with factory methods
    Reorganized MEGParent as an interface, adding two implementations: MEGBoneParent and MEGModelParent. Introduced factory methods `byBone` and `byModel` for cleaner instantiation. This improves code clarity and flexibility for handling different parent types.

--- plugin/src/main/kotlin/gg/aquatic/comet/hook/modelengine/MEGParent.kt
+++ plugin/src/main/kotlin/gg/aquatic/comet/hook/modelengine/MEGParent.kt
@@ -1,15 +1,34 @@
 package gg.aquatic.comet.hook.modelengine
 
+import com.ticxo.modelengine.api.model.ActiveModel
 import com.ticxo.modelengine.api.model.bone.ModelBone
 import gg.aquatic.comet.api.emitter.parent.Parent
 import gg.aquatic.comet.api.emitter.parent.Pose
 import gg.aquatic.comet.api.emitter.parent.pose
 
-class MEGParent(
-    val bone: ModelBone,
-): Parent {
-    override val pose: Pose
-        get() {
-            return bone.location.pose()
+interface MEGParent : Parent {
+    companion object {
+
+        fun byBone(bone: ModelBone): MEGBoneParent {
+            return MEGBoneParent(bone)
         }
+        fun byModel(model: ActiveModel): MEGModelParent {
+            return MEGModelParent(model)
+        }
+    }
+
+    class MEGBoneParent(private val bone: ModelBone) : MEGParent {
+        override val pose: Pose
+            get() {
+                return bone.location.pose()
+            }
+
+    }
+
+    class MEGModelParent(private val model: ActiveModel) : MEGParent {
+        override val pose: Pose
+            get() {
+                return model.modeledEntity.base.location.pose()
+            }
+    }
 }
