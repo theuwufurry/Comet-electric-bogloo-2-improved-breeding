c123975d823a4c2ed3d364eaff028037a0e14e7a
ruskei <dominusfinem@gmail.com>
2025-04-18 14:15:26 -0700

    boolean environment parsing

--- plugin/src/main/kotlin/gg/aquatic/comet/emitter/environment/EnvironmentDataComponent.kt
+++ plugin/src/main/kotlin/gg/aquatic/comet/emitter/environment/EnvironmentDataComponent.kt
@@ -5,11 +5,9 @@ import com.google.gson.JsonPrimitive
 import gg.aquatic.comet.api.Component
 import gg.aquatic.comet.api.emitter.EmitterComponent
 import gg.aquatic.comet.api.emitter.EmitterData
-import gg.aquatic.comet.api.emitter.environment.Datum
-import gg.aquatic.comet.api.emitter.environment.DatumNum
-import gg.aquatic.comet.api.emitter.environment.DatumStr
-import gg.aquatic.comet.api.emitter.environment.tryParseAsColor
+import gg.aquatic.comet.api.emitter.environment.*
 import gg.aquatic.comet.api.parsing.BaseComponentParser
+import gg.aquatic.comet.api.parsing.asBooleanOrNull
 import gg.aquatic.comet.api.parsing.asJsonObjectOrNull
 import gg.aquatic.comet.api.parsing.macro.Macro
 
@@ -40,6 +38,10 @@ class EnvironmentDataComponent(
                 if (!tryParseAsColor(key, element, data)) {
                     if (!element.isJsonPrimitive) continue
                     element as JsonPrimitive
+
+                    if (element.isBoolean) {
+                        data += key to DatumBool(element.asBoolean)
+                    }
 
                     if (element.isString) {
                         data += key to DatumStr(element.asString)
