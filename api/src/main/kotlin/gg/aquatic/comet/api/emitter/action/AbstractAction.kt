0b0afc016b8f825dc44669caf736936be7e4e911
ruskei <dominusfinem@gmail.com>
2025-10-08 17:01:44 -0700

    action ids

--- api/src/main/kotlin/gg/aquatic/comet/api/emitter/action/AbstractAction.kt
+++ api/src/main/kotlin/gg/aquatic/comet/api/emitter/action/AbstractAction.kt
@@ -1,10 +1,8 @@
 package gg.aquatic.comet.api.emitter.action
 
 abstract class AbstractAction {
-
     abstract val subActions: List<SubAction>
     abstract fun execute(context: ActionContext)
-
 }
 
 interface SubAction {
--- plugin/src/main/kotlin/gg/aquatic/comet/emitter/action/Action.kt
+++ plugin/src/main/kotlin/gg/aquatic/comet/emitter/action/Action.kt
@@ -5,6 +5,7 @@ import gg.aquatic.comet.api.emitter.action.AbstractAction
 import gg.aquatic.comet.api.emitter.action.ActionContext
 import gg.aquatic.comet.api.emitter.action.SubAction
 import gg.aquatic.comet.api.parsing.ComponentParser
+import gg.aquatic.comet.api.parsing.InvalidJsonException
 import gg.aquatic.comet.api.parsing.NotMyType
 import gg.aquatic.comet.api.parsing.macro.Macro
 import gg.aquatic.comet.emitter.action.sub.EmitterDieSubAction
@@ -16,7 +17,7 @@ import gg.aquatic.comet.particle.action.ParticleVanillaSpawnSubAction
 import gg.aquatic.comet.particle.action.SoundSubAction
 
 class Action(
-    override val subActions: List<SubAction>
+    override val subActions: List<SubAction>,
 ) : AbstractAction() {
     companion object {
         private val subActionParsers: MutableList<ComponentParser<out SubAction>> = mutableListOf(
@@ -29,11 +30,36 @@ class Action(
             SoundSubAction
         )
 
+        private val parserMap = subActionParsers.associateBy { it.id }
+
         fun parse(jsonArray: JsonArray, macros: Map<String, Macro>?): Result<Action> {
             val subActions: MutableList<SubAction> = mutableListOf()
             for (element in jsonArray) {
+                if (element.isJsonObject) {
+                    val obj = element.asJsonObject
+                    obj.get("action_id")?.let { id ->
+                        if (id.isJsonPrimitive && id.asJsonPrimitive.isString) {
+                            println("action_id: $id")
+                            (parserMap[id.asJsonPrimitive.asString]
+                             ?: return Result.failure(InvalidJsonException("No action has the id $id!")))
+                                .parse(element, macros)
+                                .fold(
+                                    {
+                                        subActions += it
+                                        continue
+                                    },
+                                    {
+                                        return Result.failure(it)
+                                    }
+                                )
+                        } else {
+                            return Result.failure(InvalidJsonException("Expected action_id to be a string!"))
+                        }
+                    }
+                }
+
                 subActionParsers.forEach { parser ->
-                    parser.parse(element, macros)?.fold(
+                    parser.parse(element, macros).fold(
                         { subActions += it },
                         {
                             if (it !is NotMyType) {
